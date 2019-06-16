cedc04471593b09d5b46128faea5d3f194c0aa50
Yoshiki <villager2525@gmail.com>
2019-06-16 09:45:39 +0900

    Initial Commit

---
+++ videoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  videoApp
+//
+//  Created by 西谷恭紀 on 2019/06/16.
+//  Copyright © 2019 西谷恭紀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
